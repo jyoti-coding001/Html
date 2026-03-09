e5d41ce72b0892f87ce5cad3664fc00942247832
cyberworx-frontend <devender.k@cyberworx.in>
2026-03-09 19:02:56 +0530

    JS day 1

---
+++ Js/day-1/js/app.js
@@ -0,0 +1,7 @@
+ function startAnimation() {
+    document.getElementById('sunimage').classList.add('jyoti')
+ }
+
+ function endAnimation() {
+    document.getElementById('sunimage').classList.remove('jyoti')
+ }
