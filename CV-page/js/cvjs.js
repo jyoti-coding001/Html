38367ec381d3f9f12e363871e979461f06eea1eb
jyoti-coding-001 <jyotiparjapati7053@gmail.com>
2026-03-13 16:13:19 +0530

    JS work

---
+++ CV-page/js/cvjs.js
@@ -0,0 +1,1 @@
+alert("Hi Welcome My CV Page")
---
+++ Javascript/practice/js/style.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>just for practice</title>
+    </head>
+    <body>
+        <div class="box"></div>
+        <script src="js/style.js"></script>
+
+    </body>
+</html>
