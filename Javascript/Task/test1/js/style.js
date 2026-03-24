1074d6e3ecb25b2848acf61d218c6f265fa0310e
jyoti-coding-001 <jyotiparjapati7053@gmail.com>
2026-03-24 15:29:35 +0530

    Js day 3

--- Javascript/Task/test1/js/style.js
+++ Javascript/Task/test1/js/style.js
@@ -1,8 +1,11 @@
 var boxElement = document.getElementById('boxcolor').classList
-
+var colorText = document.getElementById('colorname')
 /*function addcolor(){
 	alert("hi");
 }*/
-function addcolor(color){
-	boxElement.add(color)
+function addcolor(color, colorName){
+	boxElement.remove('pink','yellow','Green','Red','Purple','Brown','grey','light-pink','Pumpkin','Cornflower','Blue','flag-red')
+	boxElement.add(color);
+	colorText.innerText = colorName
 }
+ 
---
+++ Js/day-3/js/script.js
@@ -0,0 +1,11 @@
+var a = 10;
+let b = 20;
+const c = 30;
+
+function firstFunction() {
+    console.log("a:", a);
+    console.log("b:", b);
+    console.log("c:", c);
+}
+
+firstFunction();
