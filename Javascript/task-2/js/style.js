acead3b1ee69c3344b49e5e29c31809cfaa8a79f
jyoti-coding-001 <jyotiparjapati7053@gmail.com>
2026-03-26 15:02:40 +0530

    js day 4 practice done

---
+++ Javascript/task-2/js/style.js
@@ -0,0 +1,37 @@
+var confirmdata = confirm()
+
+    console.log(confirmdata);
+
+
+if (confirmdata == true) {
+    alert ("Welcome");
+} else {
+    alert ("Please choose the correct answer");
+}
+
+let monthday = prompt("Enter Month")
+
+    console.log(monthday)
+
+if (monthday == 1) {
+    document.write('Your selected month is January' ,'<br/>','Good')
+} else if (monthday == 2) {
+    document.write('Your selected month is February')
+} else if (monthday === '3') {
+    document.write('Your selected month is March') 
+} else if (monthday === '4') {
+    document.write('Your selected month is April')
+} else if (monthday === '5') {
+    document.write('Your selected month is May')
+} else if (monthday === '6') {
+    document.write('Your selected month is June')
+} else if (monthday === '7') {
+    document.write('Your selected month is July')
+} else if (monthday === '8') {
+    document.write('Your selected month is August')
+} else {
+    document.write('Wrooongggg')
+}
+
+    
+
---
+++ Js/day-4/js/script.js
@@ -0,0 +1,67 @@
+var a = undefined;
+// alert(a)
+const agreeMessage = 'I agree to receive recurring marketing messages from {Brand Name}. I understand I can opt-out at any time by replying STOP.';
+let confirmData = confirm(agreeMessage) // It will return boolean
+
+console.log(confirmData);
+
+if (confirmData == true) {
+    alert("Welcome");
+    let userAge = prompt("What is your age", "60"); // 60 is optional for default value in input // Return data which we are inserting
+    document.write('Your age is ', userAge, ' buddy!', '<br/>')
+} else {
+    alert("Go to hell");
+}
+
+
+
+let weekDay = prompt("Enter week day", 1)
+console.log(weekDay, "weekDay")
+console.log(typeof weekDay, "weekDay")
+
+// if (weekDay === '1') {
+//     alert("Sunday")
+// }
+// if (weekDay === '2') {
+//     alert("Monday")
+// }
+// if (weekDay === '3') {
+//     alert("Tuesday")
+// }
+// if (weekDay === '4') {
+//     alert("Wednesday")
+// }
+// if (weekDay === '5') {
+//     alert("Thursday")
+// }
+// if (weekDay === '6') {
+//     alert("Friday")
+// }
+// if (weekDay === '7') {
+//     alert("Saturday")
+// }
+
+if (weekDay === '1') {
+   document.write('Your selected week day is Sunday')
+   document.write('Good')
+} else if (weekDay === '2') {
+   document.write('Your selected week day is Monday')
+} else if (weekDay === '3') {
+   document.write('Your selected week day is Tuesday')
+} else if (weekDay === '4') {
+   document.write('Your selected week day is Wednesday')
+} else if (weekDay === '5') {
+   document.write('Your selected week day is Thursday')
+} else if (weekDay === '6') {
+   document.write('Your selected week day is Friday')
+} else if (weekDay === '7') {
+   document.write('Your selected week day is Saturday')
+} else {
+   document.write('Your can`t even think week day you Dumb')
+
+    // We can added as many as if in single if
+    //    if(1 === 1){
+    //     alert("HEY")
+    //    }
+}
+
