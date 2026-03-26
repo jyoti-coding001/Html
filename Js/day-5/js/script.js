543b09918aa12d6a4fbcf9d54fbf26ff5ed49a28
cyberworx-frontend <devender.k@cyberworx.in>
2026-03-26 16:02:30 +0530

    JS Day 5 if and for loop

---
+++ Js/day-5/js/script.js
@@ -0,0 +1,84 @@
+let userName = prompt("What is your name?")
+document.writeln(userName, '<br/>');
+
+
+let country = 'India';
+let age = 17;
+
+// And Or
+
+// Apple And Mnago = &&
+// Apple Or Mango = ||
+
+
+// if (country === 'India') {
+//    if (age >= 18) {
+//       document.write("Your can Vote!")
+//    } else {
+//       document.write("Your can not Vote!")
+//    }
+// }
+
+if (country === 'India' && age >= 18) {
+   document.write("Your can Vote!", '<br/>')
+} else {
+   document.write("Your can not Vote!", '<br/>')
+}
+
+if (country === 'India' || age >= 18) {
+   document.write("Your can Vote!", '<br/>')
+} else {
+   document.write("Your can not Vote!", '<br/>')
+}
+
+
+// Ternary Operator
+// Condition ? True : False
+country === 'India' && age >= 18 ? document.write("Your can Vote!", '<br/>') : document.write("Your can not Vote!", '<br/>');
+
+let weekDay = prompt("Enter week day", 1)
+
+// if (weekDay === '1') {
+//    document.write('Your selected week day is Sunday')
+//    document.write('Good')
+// } else if (weekDay === '2') {
+//    document.write('Your selected week day is Monday')
+// } else if (weekDay === '3') {
+//    document.write('Your selected week day is Tuesday')
+// } else if (weekDay === '4') {
+//    document.write('Your selected week day is Wednesday')
+// } else if (weekDay === '5') {
+//    document.write('Your selected week day is Thursday')
+// } else if (weekDay === '6') {
+//    document.write('Your selected week day is Friday')
+// } else if (weekDay === '7') {
+//    document.write('Your selected week day is Saturday')
+// } else {
+//    document.write('Your can`t even think week day you Dumb')
+// }
+
+// Switch Operator
+
+// switch(condition) {
+//    case condition :
+//       break;
+//    default:
+//       ''
+// }
+
+switch(weekDay) {
+   case '1':
+      document.writeln('Your selected week day is Sunday');
+   break;
+
+   case '2':
+      document.writeln('Your selected week day is Monday');
+   break;
+
+   case '3':
+      document.write('Your selected week day is Tuesday')
+   break;
+
+   default :
+   document.write('Your can`t even think week day you Dumb')
+}
---
+++ Js/day-5/js/script2.js
@@ -0,0 +1,39 @@
+// Increment Operator (++ Will add only 1) -- Will minus only 1)
+let x = 0;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+// x = x + 1;
+
+x++; // 1
+x++; // 2
+x++; // 3
+x++; // 4
+x++; // 5
+x++; // 6
+x++; // 7
+x++; // 8
+x++; // 9
+x++; // 10
+x--; // 9
+x--; // 8
+x--; // 7
+
+document.writeln(x, '<br/>')
+
+// Loop
+// type of Loop = for loop, while loop, do while loop
+
+// For Loop
+for (let i = 1; i <= 10; i++) {
+   document.writeln(i, "<br/>");
+} 
+
+// 2 ka table
+for (let i = 1; i <= 10; i++) {
+   document.writeln("2" + 'x' + i + '=' + 2 * i, "<br/>");
+}
